762342d11a3471204f4fa38ef022262646443e40
geonha081030 <geonha081030@gmail.com>
2026-03-26 22:12:45 +0900

    Update auth.js

--- auth.js
+++ auth.js
@@ -2,14 +2,10 @@
 import { auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, sendEmailVerification, onAuthStateChanged } from './firebase-config.js';
 
 export async function signUp(email, password){
-  if(!email.endsWith("@school.edu")){
-    alert("학교 이메일만 가입 가능합니다!");
-    return;
-  }
   try{
     const userCredential = await createUserWithEmailAndPassword(auth, email, password);
     await sendEmailVerification(userCredential.user);
-    alert("가입 완료! 이메일 인증 후 로그인하세요.");
+    alert("회원가입 완료! 이메일 인증 후 로그인하세요.");
   } catch(e){
     alert("회원가입 오류: "+e.message);
   }
@@ -28,10 +24,6 @@ export async function login(email, password){
   }
 }
 
-onAuthStateChanged(auth, user=>{
-  if(user){
-    console.log("로그인 상태:", user.email);
-  } else {
-    console.log("로그아웃 상태");
-  }
-});
+export function observeAuthState(callback){
+  onAuthStateChanged(auth, callback);
+}
