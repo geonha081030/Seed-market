864a89c31d653a3f26c68310d406d61c6f3e8222
geonha081030 <geonha081030@gmail.com>
2026-03-30 09:17:52 +0900

    Update product-list.js

--- product-list.js
+++ product-list.js
@@ -37,12 +37,10 @@ window.showProducts = async () => {
   });
 };
 
-// 검색
 document.getElementById("product-search").addEventListener("input", () => {
   window.showProducts();
 });
 
-// 이동
 document.getElementById("go-list-btn").addEventListener("click", () => {
   window.showProductListScreen();
 });
